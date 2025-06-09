0e5751fdca6d04d5475cb93ce311211ef6330ad4
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-06-09 01:46:19 +0100

    Second page for mobile done

--- src/components/SecondPage.tsx
+++ src/components/SecondPage.tsx
@@ -2,7 +2,7 @@ import img1 from "../assets/image_1.jpeg";
 import img2 from "../assets/image_2.jpeg";
 import img3 from "../assets/image_3.jpeg";
 import img4 from "../assets/image_4.jpeg";
-import { ButtonGreenShort } from "./Button";
+import { ButtonGreenLong } from "./Button";
 
 const SecondPage = () => {
   return (
@@ -46,14 +46,14 @@ const SecondPage = () => {
       </div>
 
       <div className="mt-24 w-full flex justify-center items-center">
-        <div className="w-[60%]">
+        <div className="md:w-[60%]">
           <p className="text-green-400 pb-4">
             TRY HULU (WITH ADS) + LIVE TV FREE FOR 3 DAYS
           </p>
-          <h1 className="text-white text-[3.3rem] font-bold pb-4">
+          <h1 className="text-white md:text-[3.3rem] text-3xl font-bold pb-4">
             Watch Live TV From 95+ Channels
           </h1>
-          <p className="text-white text-[17px] mb-4">
+          <p className="text-white md:text-[17px] mb-4 leading-tight">
             Get top national and local channels on Hulu (With Ads) + Live TV
             with your favorite live sports, news, and events - plus the entire
             Hulu streaming library. With Unlimited DVR, store Live TV recordings
@@ -61,10 +61,10 @@ const SecondPage = () => {
             Includes access to endless entertainment with Disney+ and live
             sports with ESPN+.
           </p>
-          <ButtonGreenShort text="Start your free trial" />
+          <ButtonGreenLong text="Start your free trial" />
           <p className="text-[10px] text-gray-400 my-4">Hulu (With Ads) + Live TV plan $82.99/mo. after 3-day free trial (if any) unless canceled. Cancel anytime. Regional restrictions, blackouts and Live TV terms apply. Access content from each service separately. Location data required to watch certain content. Offer valid for eligible subscribers only. See details.</p>
 
-          <p><a href="#" className="underline text-gray-300 mt-4">VIEW CHANNELS IN YOUR AREA →</a></p>
+          <p><a href="#" className="underline text-gray-300 md:text-md text-sm mt-4">VIEW CHANNELS IN YOUR AREA →</a></p>
         </div>
       </div>
     </div>
